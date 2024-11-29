9bde1c4110f497417da67aa863a6db4572952231
Bokobelin <matthieu.richelle@gmail.com>
2024-11-29 11:45:43 +0100

    Change links

--- paleohebrewdss/src/components/header.tsx
+++ paleohebrewdss/src/components/header.tsx
@@ -1,21 +1,27 @@
 import Link from "next/link";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
 
 export default function Header() {
-    return (
-        <div className="z-10 relative flex flex-col items-center justify-center gap-4 text-3xl bg-black pb-5">
-        <h1 className="z-10 text-gray-100">Paleo-Hebrew Dead Sea Scrolls</h1>
-        <div className="flex gap-4">
-          <Link href="/">
-            <Button>Home</Button>
-          </>
-          <a href="/database">
-            <Button>Database</Button>
-          </a>
-          <a href="/credits">
-            <Button>Credits</Button>
-          </a>
-        </div>
+  return (
+    <div className="z-10 relative flex flex-col items-center justify-center gap-4 text-3xl bg-black pb-5">
+      <h1 className="z-10 text-gray-100">Paleo-Hebrew Dead Sea Scrolls</h1>
+      <div className="flex gap-4">
+        <Link href="/" passHref>
+          <Button className="text-gray-300">
+            Home
+          </Button>
+        </Link>
+        <Link href="/database" passHref>
+          <Button className="text-gray-300">
+        Database
+          </Button>
+        </Link>
+        <Link href="/credits" passHref>
+          <Button className="text-gray-300">
+            Credits
+          </Button>
+        </Link>
       </div>
-    );
+    </div>
+  );
 }
